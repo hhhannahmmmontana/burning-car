3ae899870fb4a870c945208057920a8d110abbe4
hhhannahmmmontana <rubakhin.edu@niuitmo.ru>
2026-03-11 14:55:18 +0300

    feat: .env service port selection

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('NestApplication');
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.SERVICE_PORT ?? 3000;
   await app.listen(port);
   logger.log(`Server is running on port ${port}`)
 }
